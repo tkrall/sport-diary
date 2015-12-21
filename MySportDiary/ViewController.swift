75a541a3e053a8c92464d5765e93976c00a9ada6
Koulutus <psk-service@dc7-zfyjtg02z781pk4wt-3.rev.dnainternet.fi>
2015-12-21 16:32:43 +0200

    Initial Commit

---
+++ MySportDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySportDiary
+//
+//  Created by Koulutus on 21/12/15.
+//  Copyright © 2015 Koulutus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
