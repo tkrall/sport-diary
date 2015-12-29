125eeab359d97f47b1082fe00e4a1e2bb26f77d4
Koulutus <psk-service@pp.local>
2015-12-29 19:20:27 +0200

    Story board updated.

---
+++ MySportDiary/MySportUiNavigationController.swift
@@ -0,0 +1,49 @@
+//
+//  MySportUiNavigationController.swift
+//  MySportDiary
+//
+//  Created by Koulutus on 29/12/15.
+//  Copyright © 2015 Koulutus. All rights reserved.
+//
+
+
+import UIKit
+
+class MySportUiNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    func didFinishViewController(
+        viewController:MySportEntryTableViewController, didSave:Bool) {
+            
+            // Save data here from Entry controller
+            if didSave {
+                
+            }
+            
+            // Close MySportEntry window
+            dismissViewControllerAnimated(true, completion: {})
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
